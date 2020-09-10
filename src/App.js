f8e78ebfcdbd717b8843c59455b40ebc60f9bd3c
artuone83 <artur.wozniak@codetain.co>
2020-09-10 12:19:07 +0200

    comments for ssr

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from "react";
 import "./style.css";
 
-const mediaQuery = window.matchMedia('(max-width: 600px)');
+const mediaQuery = window.matchMedia('(max-width: 600px)'); // ssr will throw error
+// with gatsby.js or next.js comment line above uncomment the rest below
 
 export default function App() {
   const [isMobile, setIsMobile] = useState(mediaQuery);
 
   // useEffect(()=> {
-  //   const mediaQueryDidMountCheck = () => mediaQuery;
+  //   const mediaQueryDidMountCheck = () => window.matchMedia('(max-width: 600px)');
   //   setIsMobile(mediaQueryDidMountCheck());
   // }, []);
 
-  useEffect(()=> {
+  useEffect(() => {
+    // const mediaQuery = window.matchMedia('(max-width: 600px)');
     mediaQuery.addListener((e) => {
       if (e.matches) {
         setIsMobile(true);
