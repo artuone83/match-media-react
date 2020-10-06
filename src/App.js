18b62cf4999bd6a807731b3cdf89b768146fd1d5
artuone83 <artur.wozniak@codetain.co>
2020-10-06 10:48:01 +0200

    outside click effect

--- src/App.js
+++ src/App.js
@@ -1,43 +1,53 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef, forwardRef } from "react";
 import "./style.css";
 
 const navOptions = [
   {
     id: 1,
-    name: 'Home'
+    name: "Home"
   },
   {
     id: 2,
-    name: 'About'
+    name: "About"
   },
   {
     id: 3,
-    name: 'Contact'
-  },
+    name: "Contact"
+  }
 ];
 
 const DesktopNavigation = () => {
   return (
     <ul className="desktop-nav">
-      {navOptions.map((option) => <li key={option.name}>{option.name}</li>)}
+      {navOptions.map(option => (
+        <li key={option.name}>{option.name}</li>
+      ))}
     </ul>
   );
-}
+};
 
-const MobileNavigatoin = ({ isMobileNavOpen }) => {
+const MobileNavigatoin = forwardRef(({ isMobileNavOpen }, ref) => {
   return (
-    <ul className={`mobile-nav ${isMobileNavOpen ?  'mobile-nav--open' : 'mobile-nav--close'}`}>
-      {navOptions.map((option) => <li key={option.name}>{option.name}</li>)}
+    <ul
+      ref={ref}
+      className={`mobile-nav ${
+        isMobileNavOpen ? "mobile-nav--open" : "mobile-nav--close"
+      }`}
+    >
+      {navOptions.map(option => (
+        <li key={option.name}>{option.name}</li>
+      ))}
     </ul>
   );
-}
+});
 
-const mediaQuery = window.matchMedia('(max-width: 600px)'); // ssr will throw error
+const mediaQuery = window.matchMedia("(max-width: 600px)"); // ssr will throw error
 // with gatsby.js or next.js comment line above uncomment the rest below
 
 export default function App() {
   const [isMobile, setIsMobile] = useState(mediaQuery.matches); // remove mediaQuery.matches from useState
   const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+  const mobileNav = useRef(null);
 
   // useEffect(()=> {
   //   const mediaQueryDidMountCheck = () => window.matchMedia('(max-width: 600px)');
@@ -46,19 +56,33 @@ export default function App() {
 
   useEffect(() => {
     // const mediaQuery = window.matchMedia('(max-width: 600px)');
-    console.log('render')
-    mediaQuery.addListener((e) => {
+    console.log("render");
+    mediaQuery.addListener(e => {
       if (e.matches) {
         setIsMobile(true);
       } else {
         setIsMobile(false);
       }
-    })
+    });
   }, [isMobile]);
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside, true);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  }, [isMobileNavOpen]);
 
   const handleOpenMobileNav = () => {
     setIsMobileNavOpen(true);
-  }
+    console.log(mobileNav.current);
+  };
+
+  const handleClickOutside = event => {
+    if (mobileNav.current && !mobileNav.current.contains(event.target)) {
+      setIsMobileNavOpen(false);
+    }
+  };
 
   return (
     <div>
@@ -66,8 +90,15 @@ export default function App() {
       <header>
         {isMobile && <p onClick={handleOpenMobileNav}>MENU</p>}
         <nav>
-        {isMobile ? <MobileNavigatoin isMobileNavOpen={isMobileNavOpen}/> : <DesktopNavigation /> }
-      </nav>
+          {isMobile ? (
+            <MobileNavigatoin
+              ref={mobileNav}
+              isMobileNavOpen={isMobileNavOpen}
+            />
+          ) : (
+            <DesktopNavigation />
+          )}
+        </nav>
       </header>
     </div>
   );
